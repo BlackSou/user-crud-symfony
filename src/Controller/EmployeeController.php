5678c019f486d5b8bc24fdd8acb1de61e955f347
blacksou <itv.develop@gmail.com>
2023-08-31 01:03:04 +0300

    add method and route for create employee UCS-2

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
-class EmployeeController
-{
+use App\DTO\ApiErrorResponse;
+use App\DTO\CreateEmployeeRequest;
+use App\Service\EmployeeServiceInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class EmployeeController extends AbstractController
+{
+    public function __construct(private readonly EmployeeServiceInterface $employeeService)
+    {
+    }
+
+    #[Route('/api/v1/employee', name: 'employee', methods: ['POST'])]
+    #[OA\Tag(name: 'Employee API')]
+    #[OA\Response(response: 200, description: 'Create a employee', attachables: [new Model(type: EmployeeServiceInterface::class)])]
+    #[OA\Response(response: 400, description: 'Employee duplicate', attachables: [new Model(type: ApiErrorResponse::class)])]
+    #[OA\RequestBody(attachables: [new Model(type: CreateEmployeeRequest::class)])]
+    public function createEmployee(#[RequestBody] CreateEmployeeRequest $createEmployeeRequest): Response
+    {
+        return $this->json($this->employeeService->createEmployee($createEmployeeRequest));
+    }
 }
