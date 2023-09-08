0351ea7925c3d99e28815aeed694ffc0e0d20874
blacksou <itv.develop@gmail.com>
2023-09-08 04:06:51 +0300

    add nesting levels for error response UCS-2

--- src/DTO/ApiErrorResponse.php
+++ src/DTO/ApiErrorResponse.php
@@ -1,6 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DTO;
+
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 
 class ApiErrorResponse
 {
@@ -13,6 +18,10 @@ class ApiErrorResponse
         return $this->message;
     }
 
+    #[OA\Property(type: 'object', nullable: true, oneOf: [
+        new OA\Schema(ref: new Model(type: ApiErrorDebugDetails::class)),
+        new OA\Schema(ref: new Model(type: ApiErrorValidationDetails::class))]
+    )]
     public function getDetails(): mixed
     {
         return $this->details;
